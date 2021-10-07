49f5a19de8f93441974574fdf516e015e43fd216
Mitchell Posney <mposney@gmail.com>
2021-10-07 09:44:51 -0400

    Minor Changes

--- java/src/main/java/com/techelevator/VendingMachineCLI.java
+++ java/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -2,31 +2,45 @@ package com.techelevator;
 
 import com.techelevator.view.Menu;
 
-public class VendingMachineCLI {
+public class VendingMachineCLI
+{
 
 	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
 	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
-	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE };
+	private static final String MAIN_MENU_OPTION_EXIT = "Exit";
+	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE, MAIN_MENU_OPTION_EXIT};
 
 	private Menu menu;
 
-	public VendingMachineCLI(Menu menu) {
+	public VendingMachineCLI(Menu menu)
+	{
 		this.menu = menu;
 	}
 
-	public void run() {
-		while (true) {
+	public void run()
+	{
+		while (true)
+		{
 			String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
 
-			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
+			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS))
+			{
+
 				// display vending machine items
-			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
+			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE))
+			{
 				// do purchase
 			}
+			else if(choice.equals(MAIN_MENU_OPTION_EXIT))
+			{
+				// do exit
+			}
+
 		}
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		Menu menu = new Menu(System.in, System.out);
 		VendingMachineCLI cli = new VendingMachineCLI(menu);
 		cli.run();
--- java/src/main/java/com/techelevator/view/Menu.java
+++ java/src/main/java/com/techelevator/view/Menu.java
@@ -5,45 +5,56 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
-public class Menu {
+public class Menu
+{
 
 	private PrintWriter out;
 	private Scanner in;
 
-	public Menu(InputStream input, OutputStream output) {
+	public Menu(InputStream input, OutputStream output)
+	{
 		this.out = new PrintWriter(output);
 		this.in = new Scanner(input);
 	}
 
-	public Object getChoiceFromOptions(Object[] options) {
+	public Object getChoiceFromOptions(Object[] options)
+	{
 		Object choice = null;
-		while (choice == null) {
+		while (choice == null)
+		{
 			displayMenuOptions(options);
 			choice = getChoiceFromUserInput(options);
 		}
 		return choice;
 	}
 
-	private Object getChoiceFromUserInput(Object[] options) {
+	private Object getChoiceFromUserInput(Object[] options)
+	{
 		Object choice = null;
 		String userInput = in.nextLine();
-		try {
+		try
+		{
 			int selectedOption = Integer.valueOf(userInput);
-			if (selectedOption > 0 && selectedOption <= options.length) {
+			if (selectedOption > 0 && selectedOption <= options.length)
+			{
 				choice = options[selectedOption - 1];
 			}
-		} catch (NumberFormatException e) {
+		} catch (NumberFormatException e)
+		{
 			// eat the exception, an error message will be displayed below since choice will be null
 		}
-		if (choice == null) {
+		if (choice == null)
+		{
 			out.println(System.lineSeparator() + "*** " + userInput + " is not a valid option ***" + System.lineSeparator());
 		}
 		return choice;
 	}
 
-	private void displayMenuOptions(Object[] options) {
+	private void displayMenuOptions(Object[] options)
+	{
 		out.println();
-		for (int i = 0; i < options.length; i++) {
+		for (int i = 0; i < options.length; i++)
+		{
 			int optionNum = i + 1;
 			out.println(optionNum + ") " + options[i]);
 		}
